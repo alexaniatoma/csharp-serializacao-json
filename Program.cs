65e30a4726f221887593c05afa922f280cdacec7
alexaniatoma <alexaniatoma@hotmail.com>
2024-05-13 15:20:53 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using dioAvanade.Models;
+
+LeituraArquivo arquivo = new LeituraArquivo();
+
+var(Sucesso, linhasArquivo, quantidadeLinhas) = arquivo.LerArquivo("Arquivo/arquivoLeitura.txt");
+if(Sucesso)
+{
+    Console.WriteLine("O arquivo contém: " + quantidadeLinhas + " linhas");
+
+    foreach(string linha in linhasArquivo)
+    {
+        Console.WriteLine(linha);
+    }
+}
+    else
+    {
+        Console.WriteLine("Não foi possiviel ler o arquivo");
+    }
+
+
+
+
